4b1e1191de95217a2074c1a4f8f1cefc745ad9d6
daiyh98 <dyh9@vip.qq.com>
2023-07-09 04:13:11 +0800

    完成上传页面接口

--- handler/handler.go
+++ handler/handler.go
@@ -1,10 +1,21 @@
 package handler
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		//返回上传html页面
+		data, err := os.ReadFile("./static/view/index.html")
+		if err != nil {
+			io.WriteString(w, "internal server error")
+			return
+		} else {
+			io.WriteString(w, string(data))
+		}
 	} else if r.Method == "POST" {
 		//接收文件流及存储到本地目录
 	}
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"myFileServer/handler"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Failed to start server, error: %s", err.Error())
+	}
+}
